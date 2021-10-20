41754ef9f1d8c76fd841752eab5a5762f71dc8f6
Sanuja kumari <sanujakumari@gmail.com>
2021-10-21 02:01:10 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function clear(){
+    document.getElementById("result").value="0 "
+}
+function view(num){
+    document.getElementById("result").value+=num
+}
+function compute(){
+    let a=document.getElementById("result").value;
+    let b=eval(a)
+    document.getElementById("result").value=b
+}
